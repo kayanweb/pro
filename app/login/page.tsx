3958e997f7537de67f5430ace1910804fc57c93e
YOUR_GITHUB_rnmohamedelsayedabdallah <tqnyplus@gmail.com>
2026-05-08 18:01:03 +0300

    add simple login

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'Arial' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Pronurse 👋</h1>
+        <p>Coming soon...</p>
+      </div>
+    </div>
+  )
+}
